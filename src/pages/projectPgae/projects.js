ede2b70612623dddb538430eb348b5aa13cd2abf
zama <136614897+zamamojisola@users.noreply.github.com>
2025-08-01 11:38:18 +0100

    Update projects.js
    still on project page modification

--- src/pages/projectPgae/projects.js
+++ src/pages/projectPgae/projects.js
@@ -2,10 +2,10 @@ import paysimg from "../../assets/pays.png"
 import portfolio from "../../assets/porfolioimg.png";
 import contentApi from "../../assets/contentApiImg.png";
 export const projectList = [
-  { id: 1, image: portfolio, 
+  { id: 1, image: contentApi, 
     preview:"The E-coomerce backend manages user onboarding, products and cart management, order processes, while ensuring authorized access while using the app. ",
     link: "https://github.com/zamamojisola/alanizaneyserver",
-    title:"Al-anizaney Backend"
+    title:"Al-anizaneyBackend"
   },
   { id: 2, image: paysimg, 
     preview:"The web application allows user to sign-up and verify their email in order to buy internet data and airtime from their wallet or other payment means.",
